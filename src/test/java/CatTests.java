1e5a55d1a2540058793681fb9a26d91eda3cf5d5
Gleb Itskovich <gleb.kotlyar@yandex.ru>
2025-10-08 09:34:10 +0300

    дописан тест с моком

--- src/test/java/CatTests.java
+++ src/test/java/CatTests.java
@@ -1,34 +1,52 @@
 import com.example.Cat;
 import com.example.Feline;
+import com.example.Predator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 public class CatTests {
-    Feline feline;
 
+    Feline feline;
+    @Mock
+    Feline felineMock;
     @BeforeEach
     public void setUp() {
         feline = new Feline();
-
     }
+
     @Test
     public void getSoundShouldReturnMeow() {
-
         Cat cat = new Cat(feline);
         String actualResult = cat.getSound();
         String expectedResult = "Мяу";
-        assertEquals(actualResult, expectedResult, "Кошка говорит \"Мяу\"");
+        assertEquals(expectedResult, actualResult, "Cat says \"Meow\"");
     }
+
     @Test
     public void getFoodShouldReturnAnimalsBirdsFish() throws Exception {
         Cat cat = new Cat(feline);
         List<String> expectedList = List.of("Животные", "Птицы", "Рыба");
         List<String> actualList = cat.getFood();
-        assertEquals(actualList, expectedList, "Expected list : Animals,Birds,Fish");
+
+        assertEquals(expectedList, actualList, "Expected list : Animals,Birds,Fish");
+    }
+
+    @Test
+    public void getFoodShouldCallEatMeatMethod() throws Exception{
+        Cat cat = new Cat(felineMock);
+        List<String> expectedList = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(felineMock.eatMeat()).thenReturn(expectedList);
+        List<String> actualList = cat.getFood();
+        Mockito.verify(felineMock).eatMeat();
     }
 }
